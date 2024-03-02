724053649e7607ea42550055063ddb0fdb85178e
Cesar Muñoz <rcesarrdavid@gmail.com>
2024-03-01 19:27:40 -0500

    created main_activity.xml

--- app/src/main/java/com/componentes/sudoku/MainActivity.kt
+++ app/src/main/java/com/componentes/sudoku/MainActivity.kt
@@ -15,29 +15,6 @@ import com.componentes.sudoku.ui.theme.SudokuTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SudokuTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SudokuTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
